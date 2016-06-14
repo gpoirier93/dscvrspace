56d784f1ca03e3894fc26975980a141127265d93
guillaume <guillaume.poirier@hotmail.com>
2016-06-14 14:26:04 -0400

    add psql

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var pg = require('pg')
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,6 +13,28 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('index.ejs');
+});
+
+app.get('/db', function (request, response) {
+
+  pg.connect("postgres://qngxbqhjmbfqjc:zJhNZhA4N7K7lsQ4KUGSgzFhpR@ec2-54-243-199-137.compute-1.amazonaws.com:5432/d509q9qjjgacnc?ssl=true", function(err, client, done) {
+    if (err) {
+      console.error(err); response.send("Error " + err);
+    } else {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err)
+        {
+           console.error(err); response.send("Error " + err);
+        }
+        else
+        {
+          console.log(result.rows);
+          response.render('pages/db', {results: result.rows} );
+        }
+      });
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
